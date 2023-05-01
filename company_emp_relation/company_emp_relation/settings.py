4cf8d612be4009f1bf1e6e3a8872f705ed6b624a
Anshu-Tomar <anshuggp@gmail.com>
2023-04-30 21:43:37 -0700

    Third commit

--- company_emp_relation/company_emp_relation/settings.py
+++ company_emp_relation/company_emp_relation/settings.py
@@ -77,16 +77,9 @@ WSGI_APPLICATION = 'company_emp_relation.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
 
-
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cruddb',
-        'USER': 'root',
-        'PASSWORD': 'Godwithme@123',
-        'HOST':'localhost',
-        'PORT':'3306',
 
     }
 }
